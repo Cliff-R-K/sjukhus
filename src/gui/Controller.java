e939cd5f8a62ec121de3c28bde77edc49374234a
Cliff Karlsson <silvercircle666@gmail.com>
2018-12-05 13:40:04 +0100

    Gui_trix

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -14,21 +14,43 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import model.Radiopharmaceutical;
+import model.Room;
 import model.Supplier;
 
 public class Controller implements Initializable {
 
-	public DatePicker ankomstdatum = new DatePicker();
-	public ComboBox<Supplier> combobox_suppliers = new ComboBox<>();
 	private ObservableList<Supplier> supplierList = FXCollections.observableArrayList();
-	public ComboBox<Radiopharmaceutical> combobox_radio = new ComboBox<>();
 	private ObservableList<Radiopharmaceutical> radioList = FXCollections.observableArrayList();
+	
+	public DatePicker ankomstdatum = new DatePicker();
+	public DatePicker kalibreringsdatum = new DatePicker();
+	
+	public ComboBox<Supplier> combobox_suppliers = new ComboBox<>();
+	public ComboBox<Radiopharmaceutical> combobox_radio = new ComboBox<>();
+	public ComboBox<Room> combobox_room = new ComboBox<>();
+	
+	public Label label_rad_substance = new Label();
+	public Label label_halftime = new Label();
+	
+	public TextField text_kalibreringsaktivitet = new TextField();
+	public TextField text_kalibreringstid = new TextField();
+	public TextField text_batchnr = new TextField();
+	public TextField text_kommentar = new TextField();
 	public ListView<String> listView = new ListView<String>();
+	
+	public CheckBox check_kontamineringskontroll = new CheckBox();
 	public Button button = new Button();
+	
+
+	
+	
 	
 	
 	
@@ -45,16 +67,21 @@ public class Controller implements Initializable {
 		combobox_radio.getItems().clear();
 		combobox_radio.getItems().addAll(radioList);
 		combobox_radio.getSelectionModel().selectFirst();
-
+		
+		label_rad_substance.setText(combobox_radio.getValue().getSubstance().getName());
+		label_halftime.setText(combobox_radio.getValue().getSubstance().getHalfLife()+"");
 	}
 	
+	public void ContaminationCheck(){
+		
+	}
 	
 	public void disableElements() {
 		
 	}
 
 	public String getCurrentDate() {
-		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		Date date = new Date();
 		return dateFormat.format(date);
 	}
@@ -62,11 +89,8 @@ public class Controller implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		addSuppliers();
-		ankomstdatum.setPromptText(getCurrentDate());
+		ankomstdatum.setValue(LocalDate.now());
 		combobox_radio.setDisable(true);
-	
-		
-		
 	}
 	
 
