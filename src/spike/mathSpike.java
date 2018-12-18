c389abef45bc8c94b7c16a60c6198e3e133bd463
kallekarta <kristersundlof@gmail.com>
2018-12-18 15:31:35 +0100

    Fixat klart aktivitetskontrollen. BOOM!

--- src/spike/mathSpike.java
+++ src/spike/mathSpike.java
@@ -1,27 +1,33 @@
 package spike;
 
-import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 import controller.MathController;
-import dao.CalibrationDao;
 import dao.RadiopharmaceuticalDao;
+import dao.RegRadioDao;
 import dao.RoomDao;
 import dao.UserDao;
 import model.RegRadio;
 
 public class mathSpike {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		// TODO Auto-generated method stub
 		final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy MM dd HH mm");
 		LocalDateTime startDate = LocalDateTime.parse("2018 12 14 19 30",formatter);
+		String pattern = "yyyy-MM-dd HH:mm";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+		Date arrivalDate = simpleDateFormat.parse("2018-12-17 10:26");
 
 		System.out.println("startDate: "+startDate);
 
-		RegRadio radio = new RegRadio(1000.0, startDate, null, "hittat", "OK", new RadiopharmaceuticalDao().get(2), new RoomDao().get(1),new UserDao().get(1),new CalibrationDao().get(1)); 
-
+//		RegRadio radio = new RegRadio(1000.0, startDate, null, "hittat", "OK", new RadiopharmaceuticalDao().get(2), new RoomDao().get(1),new UserDao().get(1),new CalibrationDao().get(1)); 
+		RegRadio radio = new RegRadio(1000.0, startDate, arrivalDate, "hittat", "OK", new RadiopharmaceuticalDao().get(1), new RoomDao().get(1),new UserDao().get(1), new RegRadioDao().get(1).getCalibrationActivity(),new RegRadioDao().get(1).getCalibrationDate()); 
+		
 		MathController mCon = new MathController();
 		
 		
